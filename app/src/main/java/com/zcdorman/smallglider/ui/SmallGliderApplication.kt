f9e2108068b18bca3b4e9c3b2dd974fd8b1f5482
o7o7orz <o7o7orz@gmail.com>
2022-04-08 08:36:54 +0900

    Application class

---
+++ app/src/main/java/com/zcdorman/smallglider/ui/SmallGliderApplication.kt
@@ -0,0 +1,9 @@
+package com.zcdorman.smallglider.ui
+
+import android.app.Application
+
+class SmallGliderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
